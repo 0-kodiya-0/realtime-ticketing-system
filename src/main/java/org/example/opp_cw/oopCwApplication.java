a44c71efad64b712476eaa5e3d6366c798b27309
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-04 11:30:17 +0530

    Created spring boot project structure

---
+++ src/main/java/org/example/opp_cw/oopCwApplication.java
@@ -0,0 +1,13 @@
+package org.example.opp_cw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oopCwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(oopCwApplication.class, args);
+    }
+
+}
