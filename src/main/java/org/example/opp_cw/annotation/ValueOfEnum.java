80214273cd8477d85464d991b7559ca26b6c2463
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-05 23:23:56 +0530

    An annotation that validates if a string is equal to the values of the enums defined inside

---
+++ src/main/java/org/example/opp_cw/annotation/ValueOfEnum.java
@@ -0,0 +1,44 @@
+package org.example.opp_cw.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+import java.util.Arrays;
+
+@Documented
+@Constraint(validatedBy = ValueOfEnumValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValueOfEnum {
+    Class<? extends Enum<?>> enumClass();
+
+    String message() default "must be one of the predefined values";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
+class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, String> {
+
+    private String[] acceptedValues;
+    private String customMessage;
+
+    @Override
+    public void initialize(ValueOfEnum annotation) {
+        acceptedValues = Arrays.stream(annotation.enumClass().getEnumConstants())
+                .map(Enum::name)
+                .toArray(String[]::new);
+        customMessage = "must be one of the following values: " + Arrays.toString(acceptedValues);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(customMessage).addConstraintViolation();
+        return value == null || Arrays.asList(acceptedValues).contains(value);
+    }
+}
