9a564bf5632a64cf7828b5fef5a75e4eca1d80ed
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-05 23:17:51 +0530

    Changes are made but error exists

--- src/main/java/org/example/opp_cw/model/Admin.java
+++ src/main/java/org/example/opp_cw/model/Admin.java
@@ -2,23 +2,38 @@ package org.example.opp_cw.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.bson.types.ObjectId;
+import org.example.opp_cw.dto.Address;
 import org.example.opp_cw.enums.AccessLevel;
+import org.example.opp_cw.enums.Gender;
 import org.example.opp_cw.enums.Privileges;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
-import org.springframework.lang.NonNull;
 
+import java.util.Date;
 import java.util.List;
 
 @Document("Admin")
 @Data
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = true)
-public class Admin extends Person {
+@CompoundIndex(def = "{'name': 1, 'surname': 1}", unique = true)
+public class Admin {
     @MongoId
     private ObjectId id;
-    private @NonNull AccessLevel accessLevel;
-    private @NonNull List<Privileges> privileges;
+    private String name;
+    private String surname;
+    private int age;
+    private Gender gender;
+    private Date dateOfBirth;
+    @Indexed(unique = true)
+    private String nic;
+    private Address address;
+    private String nationality;
+    private AccessLevel accessLevel;
+    private List<Privileges> privileges;
+    private boolean isAuthorized = false;
+    private boolean isVisible = false;
+    private boolean isDeleted = false;
 }
