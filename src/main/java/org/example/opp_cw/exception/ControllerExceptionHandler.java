c0baee987a7c019d2dcbc1c0a6334903f8ca1a0b
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-05 23:27:32 +0530

    Created to handle the validation error happen in controller package

---
+++ src/main/java/org/example/opp_cw/exception/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.example.opp_cw.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice(basePackages = "org.example.opp_cw.controller")
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(violation -> {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
