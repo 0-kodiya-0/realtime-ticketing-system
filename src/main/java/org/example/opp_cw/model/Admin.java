01f074a6bc2cfd3e247b1ba949d924d66b8ca8ba
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-06 11:34:30 +0530

    AccessLevel and privileges are not tested

--- src/main/java/org/example/opp_cw/model/Admin.java
+++ src/main/java/org/example/opp_cw/model/Admin.java
@@ -1,39 +1,21 @@
 package org.example.opp_cw.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.bson.types.ObjectId;
-import org.example.opp_cw.dto.Address;
 import org.example.opp_cw.enums.AccessLevel;
-import org.example.opp_cw.enums.Gender;
 import org.example.opp_cw.enums.Privileges;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.util.Date;
 import java.util.List;
 
 @Document("Admin")
 @Data
-@AllArgsConstructor
-@CompoundIndex(def = "{'name': 1, 'surname': 1}", unique = true)
-public class Admin {
+@EqualsAndHashCode(callSuper = true)
+public class Admin extends Person {
     @MongoId
     private ObjectId id;
-    private String name;
-    private String surname;
-    private int age;
-    private Gender gender;
-    private Date dateOfBirth;
-    @Indexed(unique = true)
-    private String nic;
-    private Address address;
-    private String nationality;
     private AccessLevel accessLevel;
     private List<Privileges> privileges;
-    private boolean isAuthorized = false;
-    private boolean isVisible = false;
-    private boolean isDeleted = false;
 }
