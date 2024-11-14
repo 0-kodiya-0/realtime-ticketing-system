ff2c1e0beafaaf9e691b0ecf52a1616b82d40c63
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-11-14 11:13:49 +0530

    Added functions for sign up and login processes

--- src/main/java/org/example/opp_cw/controller/SignUpController.java
+++ src/main/java/org/example/opp_cw/controller/SignUpController.java
@@ -2,44 +2,70 @@ package org.example.opp_cw.controller;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.bson.types.ObjectId;
+import org.example.opp_cw.configuration.JwtUtil;
+import org.example.opp_cw.dto.CustomAuthenticationToken;
 import org.example.opp_cw.dto.requestbody.AdminSignUpRequest;
 import org.example.opp_cw.dto.requestbody.CustomerSignUpRequest;
 import org.example.opp_cw.dto.responsebody.ApiResponse;
-import org.example.opp_cw.model.Customer;
-import org.example.opp_cw.services.CustomerService;
+import org.example.opp_cw.enums.TokenType;
 import org.example.opp_cw.services.SignUpService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/signup")
 public class SignUpController {
 
-    private final CustomerService customerService;
     private final SignUpService signUpService;
+    private final JwtUtil jwtUtil;
 
-    public SignUpController(CustomerService customerService, SignUpService signUpService) {
-        this.customerService = customerService;
+    public SignUpController(SignUpService signUpService, JwtUtil jwtUtil) {
         this.signUpService = signUpService;
+        this.jwtUtil = jwtUtil;
     }
 
-    @GetMapping("/customer")
-    public ResponseEntity<List<Customer>> getCustomer() {
-        return ResponseEntity.ok(customerService.findAll());
+    @GetMapping
+    public ResponseEntity<ApiResponse> getSignUpToken(HttpServletRequest request, HttpServletResponse response) {
+
+        String jwtToken = jwtUtil.buildToken(Map.of("TOKEN_TYPE", TokenType.SIGNUP_TOKEN.name(), "TOKEN_ID", ObjectId.get().toHexString()), "Signup token");
+        response.addHeader("Authorization", "Bearer " + jwtToken);
+        ApiResponse apiResponse = new ApiResponse(HttpStatus.OK, "signup token send", request);
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
+    }
+
+    @GetMapping("/customer/verify")
+    public ResponseEntity<ApiResponse> verifyCustomer(HttpServletRequest request) {
+        CustomAuthenticationToken authentication = (CustomAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        ApiResponse apiResponse = signUpService.verifySignUpCustomer((String) authentication.getJwtClaims().get("TOKEN_ID"), request);
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
+    }
+
+    @GetMapping("/admin/verify")
+    public ResponseEntity<ApiResponse> verifyAdmin(HttpServletRequest request) {
+        CustomAuthenticationToken authentication = (CustomAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        ApiResponse apiResponse = signUpService.verifySignUpAdmin((String) authentication.getJwtClaims().get("TOKEN_ID"), request);
+        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
 
     @PostMapping("/customer")
     public ResponseEntity<ApiResponse> signupCustomer(@Valid @RequestBody CustomerSignUpRequest customerSignUpRequest, HttpServletRequest request) {
-        ApiResponse apiResponse = signUpService.signUp(customerSignUpRequest, request);
+        CustomAuthenticationToken authentication = (CustomAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        ApiResponse apiResponse = signUpService.signUp((String) authentication.getJwtClaims().get("TOKEN_ID"), customerSignUpRequest, request);
         return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
 
     @PostMapping("/admin")
     public ResponseEntity<ApiResponse> signupAdmin(@Valid @RequestBody AdminSignUpRequest adminSignUpRequest, HttpServletRequest request) {
-        ApiResponse apiResponse = signUpService.signUp(adminSignUpRequest, request);
+        CustomAuthenticationToken authentication = (CustomAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        ApiResponse apiResponse = signUpService.signUp((String) authentication.getJwtClaims().get("TOKEN_ID"), adminSignUpRequest, request);
         return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
 
--- src/main/java/org/example/opp_cw/services/LoginService.java
+++ src/main/java/org/example/opp_cw/services/LoginService.java
@@ -4,8 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.example.opp_cw.dto.requestbody.LoginRequest;
 import org.example.opp_cw.dto.responsebody.ApiResponse;
 import org.example.opp_cw.model.Credentials;
-import org.example.opp_cw.repository.customer.CustomerCredentialsRepository;
-import org.example.opp_cw.repository.customer.CustomerRepository;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,24 +12,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 
-    private final CustomerCredentialsRepository customerCredentialsRepository;
-    private final CustomerRepository customerRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public LoginService(CustomerCredentialsRepository customerCredentialsRepository, CustomerRepository customerRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.customerCredentialsRepository = customerCredentialsRepository;
-        this.customerRepository = customerRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AdminService adminService;
+    private final CustomerService customerService;
+
+    public LoginService(BCryptPasswordEncoder bCryptPasswordEncoder, AdminService adminService, CustomerService customerService) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.adminService = adminService;
+        this.customerService = customerService;
     }
 
-    public ApiResponse login(LoginRequest loginRequest, HttpServletRequest request) {
+    public ApiResponse loginCustomer(LoginRequest loginRequest, HttpServletRequest request) {
         try {
-            Credentials credentials = customerCredentialsRepository.findByUserName(loginRequest.getCredentials().getUserName());
+            Credentials credentials = customerService.isCustomer(loginRequest.getCredentials().getUserName());
             if (credentials == null) {
                 return new ApiResponse(HttpStatus.NOT_FOUND, "credentials not found", request);
             }
-            if (!customerRepository.existsById(credentials.get_id())) {
+            if (!customerService.isCustomer(credentials.get_id())) {
                 return new ApiResponse(HttpStatus.NOT_FOUND, "customer not found", request);
+            }
+            if (!customerService.isCustomerVerified(credentials.get_id())) {
+                return new ApiResponse(HttpStatus.FORBIDDEN, "customer found, not verified", request);
+            }
+            if (!bCryptPasswordEncoder.matches(loginRequest.getCredentials().getPassword(), credentials.getPassword())) {
+                return new ApiResponse(HttpStatus.UNAUTHORIZED, "password invalid", request);
+            }
+            return new ApiResponse(HttpStatus.OK, "login success full", request);
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal db server error", request);
+        }
+    }
+
+    public ApiResponse loginAdmin(LoginRequest loginRequest, HttpServletRequest request) {
+        try {
+            Credentials credentials = adminService.isAdmin(loginRequest.getCredentials().getUserName());
+            if (credentials == null) {
+                return new ApiResponse(HttpStatus.NOT_FOUND, "credentials not found", request);
+            }
+            if (!adminService.isAdmin(credentials.get_id())) {
+                return new ApiResponse(HttpStatus.NOT_FOUND, "admin not found", request);
+            }
+            if (!adminService.isAdminVerified(credentials.get_id())) {
+                return new ApiResponse(HttpStatus.FORBIDDEN, "admin found, not verified", request);
             }
             if (!bCryptPasswordEncoder.matches(loginRequest.getCredentials().getPassword(), credentials.getPassword())) {
                 return new ApiResponse(HttpStatus.UNAUTHORIZED, "password invalid", request);
