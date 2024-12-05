fed495595b05d30596d40fb2e0f89526bca572d7
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-12-05 09:15:29 +0530

    Changes

--- src/main/java/org/backend/server/microservices/authorization/repository/CustomerRepository.java
+++ src/main/java/org/backend/server/microservices/authorization/repository/CustomerRepository.java
@@ -2,11 +2,20 @@ package org.backend.server.microservices.authorization.repository;
 
 import org.backend.server.microservices.authorization.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    boolean existsByCredentialsUserNameContaining(String userName);
-    Customer findByCredentialsUserNameContaining(String userName);
+    boolean existsByCredentialsUserName(String userName);
+
+    Customer findByCredentialsUserName(String userName);
+
     Optional<Customer> findById(Long id);
+
+    @Modifying
+    @Query("UPDATE Customer e SET e.credentials.authority = ?2  WHERE e.id = ?1")
+    int updateByIdAndCredentialsAuthority(Long id, List<String> authority);
 }
