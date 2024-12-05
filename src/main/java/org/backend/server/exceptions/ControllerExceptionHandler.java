b87180e8ed5b8c1766fdcd245694d53f28831675
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-12-05 16:13:57 +0530

    Added an exception which is dedicated for that service

--- src/main/java/org/backend/server/exceptions/ControllerExceptionHandler.java
+++ src/main/java/org/backend/server/exceptions/ControllerExceptionHandler.java
@@ -1,23 +1,19 @@
 package org.backend.server.exceptions;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.validation.ConstraintViolationException;
 import org.backend.server.dto.ApiResponse;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.security.auth.login.AccountException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(1)
 @ControllerAdvice(basePackages = {
         "org.backend.server.microservices.ticketpool.controller",
         "org.backend.server.microservices.authorization.controller"
@@ -29,40 +25,25 @@ public class ControllerExceptionHandler {
         return new ApiResponse(HttpStatus.BAD_REQUEST, ex.getMessage()).createResponse();
     }
 
-    @ExceptionHandler(AccountException.class)
-    public ResponseEntity<ApiResponse> handleAccountException(AccountException ex) {
-        return new ApiResponse(HttpStatus.UNAUTHORIZED, ex.getMessage()).createResponse();
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ApiResponse> handleBadCredentialsException(BadCredentialsException ex) {
-        return new ApiResponse(HttpStatus.BAD_REQUEST, ex.getMessage()).createResponse();
-    }
-
-    @ExceptionHandler(JwtException.class)
-    public ResponseEntity<ApiResponse> handleJwtException(JwtException ex) {
-        return new ApiResponse(HttpStatus.UNAUTHORIZED, ex.getMessage()).createResponse();
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
+        Map<String, Object> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(violation -> {
             String fieldName = violation.getField();
             String errorMessage = violation.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return new ApiResponse(HttpStatus.BAD_REQUEST, String.valueOf(errors)).createResponse();
+        return new ApiResponse(HttpStatus.BAD_REQUEST, errors).createResponse();
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ApiResponse> handleConstraintViolationExceptions(ConstraintViolationException ex) {
-        Map<String, String> errors = new HashMap<>();
+        Map<String, Object> errors = new HashMap<>();
         ex.getConstraintViolations().forEach(violation -> {
             String fieldName = violation.getPropertyPath().toString();
             String errorMessage = violation.getMessage();
             errors.put(fieldName, errorMessage);
         });
-        return new ApiResponse(HttpStatus.BAD_REQUEST, String.valueOf(errors)).createResponse();
+        return new ApiResponse(HttpStatus.BAD_REQUEST, errors).createResponse();
     }
 }
---
+++ src/main/java/org/backend/server/microservices/authorization/exception/AuthServiceExceptionHandler.java
@@ -0,0 +1,45 @@
+package org.backend.server.microservices.authorization.exception;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.backend.server.dto.ApiResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.security.auth.login.AccountException;
+
+@Order(2)
+@ControllerAdvice(basePackages = {
+        "org.backend.server.microservices.authorization.controller"
+})
+public class AuthServiceExceptionHandler {
+    @ExceptionHandler(AccountException.class)
+    public ResponseEntity<ApiResponse> handleAccountException(AccountException ex) {
+        return new ApiResponse(HttpStatus.UNAUTHORIZED, ex.getMessage()).createResponse();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiResponse> handleBadCredentialsException(BadCredentialsException ex) {
+        return new ApiResponse(HttpStatus.BAD_REQUEST, ex.getMessage()).createResponse();
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiResponse> handleJwtException(JwtException ex) {
+        return new ApiResponse(HttpStatus.UNAUTHORIZED, ex.getMessage()).createResponse();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ApiResponse(HttpStatus.NOT_FOUND, ex.getMessage()).createResponse();
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ApiResponse> handleEntityExistsException(EntityExistsException ex) {
+        return new ApiResponse(HttpStatus.CONFLICT, ex.getMessage()).createResponse();
+    }
+}
