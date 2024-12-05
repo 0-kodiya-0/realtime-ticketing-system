e56bcd993246819a5b1fd4fc3ca8d1988f3e87db
0-kodiya-0 <sanithujayafiverr@gmail.com>
2024-12-05 14:39:29 +0530

    Added a function so that a json stringified response can be created

--- src/main/java/org/backend/server/dto/ApiResponse.java
+++ src/main/java/org/backend/server/dto/ApiResponse.java
@@ -1,18 +1,20 @@
 package org.backend.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class ApiResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final Date timestamp = new Date();
     private HttpStatus status;
     private Map<String, Object> details;
 
@@ -29,5 +31,9 @@ public class ApiResponse {
 
     public ResponseEntity<ApiResponse> createResponse() {
         return new ResponseEntity<>(this, status);
+    }
+
+    public String createJsonResponse() throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(this);
     }
 }
